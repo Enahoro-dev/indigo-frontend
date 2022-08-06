fb23ef290c23a599704b4d1adc61b5bf68821ae4
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-08-06 10:09:58 +0100

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,15 +10,14 @@ interface Props {
 
 const Layout:NextPage<Props> = ({children}) => {
     return (
-        <div className='fixed h-screen w-screen bg-main font-semibold flex items-center justify-center z-10 tablet:hidden'><h2>Please open this website on a larger screen (laptop or desktop) to view it.</h2>
-            <div className='w-screen h-screen hidden tablet:block'>
-                <Header/>
-                <div className='h-layout pl-8 bg-back flex items-center'>
-                    <Nav/>
-                    <div className='h-layout w-3/5 mx-12'>{children}</div>
-                    <Search/>
-                </div>            
-            </div>
+        <div className='w-screen h-screen'>
+            <div className='fixed h-full w-full text-center bg-main font-semibold flex items-center justify-center z-10 tablet:hidden'><h2>Please open this website on a larger screen (laptop or desktop) to view it.</h2></div>
+            <Header/>
+            <div className='h-layout pl-8 bg-back flex items-center'>
+                <Nav/>
+                <div className='h-layout w-3/5 mx-12'>{children}</div>
+                <Search/>
+            </div>            
         </div>
     )
 }
